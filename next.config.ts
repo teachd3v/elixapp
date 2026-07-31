e9791c1ac6b004c01ea2b8ff02953d70579a140c
teachd3v <teach.d3v@gmail.com>
2026-07-31 10:59:15 +0700

    chore: revert next.config.ts changes

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingExcludes: {
-    '*': [
-      'node_modules/@swc/core-linux-x64-gnu',
-      'node_modules/@swc/core-linux-x64-musl',
-      'node_modules/@esbuild/linux-x64',
-    ],
-  },
 };
 
 export default nextConfig;
