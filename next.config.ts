e3236eb0132efe9a15b6b75ad87e9e34cd7246f2
teachd3v <teach.d3v@gmail.com>
2026-07-31 10:42:40 +0700

    chore: reduce serverless function size limit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
